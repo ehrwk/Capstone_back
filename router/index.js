d52a088e7350f1849043bc4aeec9304ad5aad240
ehrwk <yundbs2001@naver.com>
2022-04-30 20:25:34 +0900

    router수정

--- router/index.js
+++ router/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-const email = require("./email");
 const user = require("./user");
 const auth = require("./auth");
 const follow = require("./follow");
 
-router.use("/email", email);
 router.use("/user", user);
 router.use("/auth", auth);
 router.use("/follow", follow);
